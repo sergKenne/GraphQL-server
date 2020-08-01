cd904b7fc82de4fd9b41366479ab777ee5bcdf4b
Kenne Serge <kenne_serge@mail.ru>
2020-08-01 04:08:46 +0300

    fetching data from node server

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
-const { graphql } = require("graphql");
+const { graphql, GraphQLList } = require("graphql");
+const axios = require('axios');
 
 const {
     GraphQLObjectType,
@@ -8,12 +9,12 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-const customers = [
-  { id: '1', name: 'serge kenne', email: 'sergekenn@mail.ru', age: 38 },
-  { id: '2', name: 'savanna Will', email: 'willysavana@gmail.com', age: 48 },
-  { id: '3', name: 'Theosmith', email: 'theosmith@yahoo.fr', age: 25 },
-  { id: '4', name: 'skenne', email: 'skenn@mail.ru', age: 36 },
-];
+// const customers = [
+//   { id: '1', name: 'serge kenne', email: 'sergekenn@mail.ru', age: 38 },
+//   { id: '2', name: 'savanna Will', email: 'willysavana@gmail.com', age: 48 },
+//   { id: '3', name: 'Theosmith', email: 'theosmith@yahoo.fr', age: 25 },
+//   { id: '4', name: 'skenne', email: 'skenn@mail.ru', age: 36 },
+// ];
 
 //CustomerType
 
@@ -38,7 +39,18 @@ const RootQuery = new GraphQLObjectType({
                 id: {type: GraphQLString}
             },
             resolve(parentValue, args) {
-                return customers.find(cust => cust.id === args.id);
+                //return customers.find(cust => cust.id === args.id);
+                return axios
+                  .get(`http://localhost:5000/api/customers/${args.id}`)
+                  .then((res) => res.data);
+                    
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return axios.get('http://localhost:5000/api/customers')
+                            .then(res => res.data);
             }
         }
     }
